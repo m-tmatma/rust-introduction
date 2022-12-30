1ed2a4d1ed75970d1a0be6ed3f5218fd0f9b3605
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-30 14:28:30 +0900

    divide lines

--- chap03/code3-41/src/main.rs
+++ chap03/code3-41/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let num = 10;
-    let add_one = |x| { num + x };
+    let add_one = |x| {
+        num + x
+    };
     let add_two = |x, y | { x + y };
     let ans = add_one(1);
     println!("ans is {}", ans);
