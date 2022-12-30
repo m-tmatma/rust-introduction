c922675f78dec83855767061f54542dfce4dea4e
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-30 14:23:32 +0900

    add sample

---
+++ chap03/code3-39/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let a = Sample::new(10);
+    let ans = a.inc();
+    println!("ans is {}", ans);
+    let ans = a.add(20);
+    println!("ans is {}", ans);
+}
+
+struct Sample {
+    x : i32,
+}
+
+impl Sample {
+    fn new(x: i32) -> Sample {
+        Sample { x: x }
+    }
+
+    fn inc(&self) -> i32 {
+        self.x + 1
+    }
+
+    fn add(&self, x: i32 ) -> i32 {
+        self.x + x
+    }
+}
