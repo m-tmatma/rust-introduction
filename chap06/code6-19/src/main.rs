d56ff14111635993235c416ce0a43db2fbe97a7d
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-01-29 20:11:46 +0900

    build

---
+++ chap06/code6-19/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    print!("LOOP is ");
+    let mut i = 0;
+    loop {
+        if i >= 10 {
+            break;
+        }
+        print!("{} ", i);
+        i += 1;
+    }
+    println!("");
+}
