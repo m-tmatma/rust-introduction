978c3cd8c3bdf98f6729a1e7c1829239563ffb73
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-01-29 17:53:36 +0900

    chaptor 05

---
+++ chap05/code5-26/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut v = vec![1,2,3,4,5];
+    vec_change(&mut v);
+
+    for i in v {
+        println!("{} ", i);
+    }
+}
+
+fn vec_change( v: &mut Vec<i32> ){
+    println!("called vec_change");
+    for i in v {
+        *i = *i * 10;
+    }
+}
