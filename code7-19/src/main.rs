c9b0f8bcc3e3d5c4b49cad06415e945228c9256d
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-03-12 10:40:42 +0900

    add

---
+++ code7-19/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let a = vec![1, 2, 3];
+    let b = vec![4, 5];
+    let v = [a, b].concat();
+    println!("v.len is {}", v.len());
+    for i in v {
+        println!("{} ", i);
+    }
+    println!("");
+}
