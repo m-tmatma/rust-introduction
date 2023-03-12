168361325badb956fbb032f90df6ba44a752c1aa
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-03-12 10:38:47 +0900

    add

---
+++ code7-18/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut v = vec![1, 2, 3, 4, 5];
+    println!("v.first is {:?}", v.first());
+    v.insert(0, 10);
+    println!("v.first is {:?}", v.first());
+    v.insert(v.len(), 99);
+    println!("v.last is {:?}", v.last());
+
+}
