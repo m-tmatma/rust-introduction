4808a7fcbc4de310be2157ce84430e26e042e037
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-03-12 10:02:59 +0900

    add

---
+++ code7-4/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let ary = ["one", "two", "three", "four", "five"];
+    println!("ary[0] is {}", ary[0]);
+    println!("ary[4] is {}", ary[4]);
+    println!("ary.len is {}", ary.len());
+}
