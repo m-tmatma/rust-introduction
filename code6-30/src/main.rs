e6e67b62f8c7702a01d35a52140651919e65c455
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-03-12 09:49:55 +0900

    add

---
+++ code6-30/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = Some(10);
+    match x {
+        Some(i) => println!("i is {}", i),
+        _ => (),
+    };
+    if let Some(i) = x {
+        println!("i is {}", i);
+    }
+}
