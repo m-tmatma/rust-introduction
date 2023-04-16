b52747981e6e83822d81e54ffdfd54407418f861
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-04-16 10:09:38 +0900

    add code7-24

---
+++ code7-24/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let v = vec![1,2,3,4,5];
+    let mut p = v.iter();
+    println!("p is {:?}", p);
+    println!("p is {:?}", p.next());
+    println!("p is {:?}", p.next());
+    println!("p is {:?}", p.next());
+    println!("p is {:?}", p.next());
+    println!("p is {:?}", p.next());
+    println!("p is {:?}", p.next());
+}
