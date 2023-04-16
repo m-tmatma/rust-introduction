b65b9c6ef177f7e00823c7f27a8968b8fd454a4a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-04-16 10:24:44 +0900

    sample

---
+++ chap08/code8-3/src/main.rs
@@ -0,0 +1,15 @@
+struct Person {
+    id: i32,
+    name: String,
+    age: i32,
+    addr: String,
+}
+
+fn main() {
+    let pa = Person {
+        id: 100,
+        name: String::from("masuda"),
+        age: 50,
+        addr: String::from("Tokyo"),
+    };
+}
