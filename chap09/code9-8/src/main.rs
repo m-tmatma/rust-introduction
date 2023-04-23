a90271a4d9f3ee6c7bde08b86e1466f689e4483b
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-04-23 17:38:40 +0900

    sample

---
+++ chap09/code9-8/src/main.rs
@@ -0,0 +1,52 @@
+struct Rectangle {
+    width: f32,
+    height: f32,
+}
+
+struct Triangle {
+    base: f32,
+    height: f32,
+}
+
+struct Circle {
+    radius: f32
+}
+
+trait CalcArea {
+    fn calc_area(&self) -> f32;
+}
+
+impl CalcArea for Rectangle {
+    fn calc_area(&self) -> f32 {
+        self.width * self.height
+    }
+}
+
+impl CalcArea for Triangle {
+    fn calc_area(&self) -> f32 {
+        self.base * self.height
+    }
+}
+
+impl CalcArea for Circle {
+    fn calc_area(&self) -> f32 {
+        self.radius * self.radius
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 10.0,
+        height: 20.0,
+    };
+    let tri = Triangle {
+        base: 10.0,
+        height: 20.0,
+    };
+    let cir = Circle {
+        radius: 10.0
+    };
+    println!("rect area is {}", rect.calc_area());
+    println!("tri area is {}", tri.calc_area());
+    println!("cir area is {}", cir.calc_area());
+}
