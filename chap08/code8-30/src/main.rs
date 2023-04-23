e545288b845268e64199ee7de8f1d1e2646b83d8
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-04-23 16:39:38 +0900

    sample

---
+++ chap08/code8-30/src/main.rs
@@ -0,0 +1,17 @@
+use std::num::ParseIntError;
+type Result<T> = std::result::Result<T, ParseIntError>;
+
+fn half_number(s: &str) -> Result<i32> {
+    s.parse::<i32>().map(|n| n / 2)
+}
+
+fn main() {
+    match half_number("100") {
+        Ok(n) => println!("Ok: {}", n),
+        Err(err) => println!("Error: {:?}", err),
+    }
+    match half_number("xxx") {
+        Ok(n) => println!("Ok: {}", n),
+        Err(err) => println!("Error: {:?}", err),
+    }
+}
