b6f1b9968e754dbe6986fb67439f7105c477f7c5
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-05-01 08:48:46 +0900

    sample

---
+++ chap12/code12-12/src/main.rs
@@ -0,0 +1,13 @@
+use std::fs::File;
+use std::io::Read;
+use std::io::{BufRead, BufReader};
+
+fn main() -> std::io::Result<()> {
+    let path = "sample.txt";
+    println!("read every one line.");
+    let file = File::open(path)?;
+    for line in BufReader::new(file).lines() {
+        println!("line is {}", line?)
+    }
+    Ok(())
+}
