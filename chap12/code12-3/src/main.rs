6f623d785f2b6b57c894731523f66162a46370d5
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-05-01 08:18:01 +0900

    sample

---
+++ chap12/code12-3/src/main.rs
@@ -0,0 +1,8 @@
+use std::fs::*;
+fn main() {
+    let path = "sample.txt";
+    println!("read all lines");
+    if let Ok(data) = read_to_string(path) {
+        println!("data is {}", data);
+    }
+}
