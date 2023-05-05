555bf3a6641f7e0fe7554b09a6da55f321a4920e
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-05-05 21:35:24 +0900

    sample

---
+++ chap13/code13-17/src/main.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    age: i32,
+    addr: String,
+}
+
+fn main() {
+    let p = Person {
+        id: 100,
+        name: String::from("masuda"),
+        age: 50,
+        addr: String::from("Tokyo"),
+    };
+    dbg!(p);
+}
