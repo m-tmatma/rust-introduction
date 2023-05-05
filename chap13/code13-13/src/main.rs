a03db3ac116e874e73bd1ff89acfff6786958ed8
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-05-05 21:15:53 +0900

    sample

---
+++ chap13/code13-13/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, `{:8} world", "rust");
+    println!("Hello, `{:<8} world", "rust");
+    println!("Hello, `{:>8} world", "rust");
+    println!("Hello, `{:^8} world", "rust");
+    println!("Hello, `{:8} world", 123);
+    println!("Hello, `{:<8} world", 123);
+    println!("Hello, `{:>8} world", 123);
+    println!("Hello, `{:^8} world", 123);
+
+}
