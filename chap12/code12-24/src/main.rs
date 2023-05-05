49125ff214645f3ffb666f6577a4b5c8bb8dcd46
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-05-05 14:39:19 +0900

    標準入力から読み込み

--- chap12/code12-24/src/main.rs
+++ chap12/code12-24/src/main.rs
@@ -6,7 +6,11 @@ use std::env;
 fn main() {
     let args = env::args().collect::<Vec<String>>();
     if args.len() <= 1 {
-
+        let reader = BufReader::new(std::io::stdin());
+        let mut writer = BufWriter::new(std::io::stdout());
+        for it in reader.bytes() {
+            writer.write(&[it.unwrap()]);
+        }
     } else {
         let file = File::open(&args[1]).expect("file not found.");
         let reader = BufReader::new(file);
