ec312cfec13ee2aab9e021785a5f53c6f17db15c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-05-05 21:43:17 +0900

    sample

---
+++ chap13/code13-20/src/main.rs
@@ -0,0 +1,22 @@
+struct Person {
+    id: i32,
+    name: String,
+    age: i32,
+    addr: String,
+}
+
+fn main() {
+    let p = Person {
+        id: 100,
+        name: String::from("masuda"),
+        age: 50,
+        addr: String::from("Tokyo"),
+    };
+    dbg!(p);
+}
+
+impl std::fmt::Debug for Person {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{ {}: `{}` in {} }}", self.id, self.name, self.addr)
+    }
+}
