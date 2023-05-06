b2e5c7effd0d7db51503c3e2abee8300bfa65eff
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-05-06 16:41:52 +0900

    sample

---
+++ chap14/code14-8/src/main.rs
@@ -0,0 +1,22 @@
+use reqwest::StatusCode;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let n = 7;
+    let url = "http://openccpm.com/blog/unknown.txt";
+    println!("call {}", url);
+    let res = reqwest::get( url ).await?;
+    match res.status() {
+        StatusCode::OK => {
+            let body = res.text().await?;
+            println!("response is \n{}", body);
+        },
+        StatusCode::NOT_FOUND => {
+            println!("error: 目的のページがありませんでした。")
+        },
+        _ => {
+            println!("error: その他のエラーが発生しました。")
+        }
+    }
+    Ok(())
+}
