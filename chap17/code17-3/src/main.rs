941ee147c9722c48a0992e1812a9f05a8742649b
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-05-07 09:50:55 +0900

    Revert "test"
    This reverts commit 1dcc07b4406d263f2e5e9f8fcb99190df2dd3ad7.

--- chap17/code17-3/src/main.rs
+++ chap17/code17-3/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("use find");
     let a = [1, 2, 3, 4, 5];
-    let b = a.iter().find(|&x| *x == 3 );
-    let c = a.iter().find(|&x| *x > 10 );
+    let b = a.iter().find(|&&x| x == 3 );
+    let c = a.iter().find(|&&x| x > 10 );
     println!("it is {:?}", b);
     println!("it is {:?}", c);
 }
