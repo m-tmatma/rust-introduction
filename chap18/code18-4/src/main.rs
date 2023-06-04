465b6c9ef80ed73de3cb9c9b2f188bbd390049b0
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-06-04 14:26:43 +0900

    add

---
+++ chap18/code18-4/src/main.rs
@@ -0,0 +1,28 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 0..10 {
+            println!("thread #1 count {}.", i);
+            thread::sleep(Duration::from_millis(1000));
+        }
+    });
+    let handle2 = thread::spawn(|| {
+        for i in 0..10 {
+            println!("thread #2 count {}.", i);
+            thread::sleep(Duration::from_millis(2000));
+        }
+    });
+    let handle3 = thread::spawn(|| {
+        for i in 0..10 {
+            println!("thread #3 count {}.", i);
+            thread::sleep(Duration::from_millis(1500));
+        }
+    });
+    println!("press enter key.");
+    handle.join().unwrap();
+    handle2.join().unwrap();
+    handle3.join().unwrap();
+    println!("program end.");
+}
