70efa59be53ef39f1e7976fb939e7d69774d4ade
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-01-04 09:06:44 +0900

    sample

--- chap15/code15-13/src/main.rs
+++ chap15/code15-13/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{params, Connection, Result};
+
+fn main() -> Result<()> {
+    let cn = Connection::open_in_memory()?;
+
+    cn.execute("CREATE TABLE users (id INTEGER, name TEXT, age INTEGER)", params![])?;
+    cn.execute("INSERT INTO users (id, name, age) VALUES (1, 'Kongo', 20)", params![])?;
+    cn.execute("INSERT INTO users (id, name, age) VALUES (1, 'Hieai', 20)", params![])?;
+    cn.execute("INSERT INTO users (id, name, age) VALUES (1, 'Haruna', 18)", params![])?;
+    cn.execute("INSERT INTO users (id, name, age) VALUES (1, 'Kirishima', 15)", params![])?;
+    Ok(())
 }
