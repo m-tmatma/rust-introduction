d727c58ad71d949e34fd30470afd102ffac0a7bf
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-01-04 09:22:52 +0900

    change

--- chap15/code15-13/src/main.rs
+++ chap15/code15-13/src/main.rs
@@ -4,9 +4,9 @@ fn main() -> Result<()> {
     let cn = Connection::open_in_memory()?;
 
     cn.execute("CREATE TABLE users (id INTEGER, name TEXT, age INTEGER)", params![])?;
-    cn.execute("INSERT INTO users (id, name, age) VALUES (1, 'Kongo', 20)", params![])?;
-    cn.execute("INSERT INTO users (id, name, age) VALUES (1, 'Hieai', 20)", params![])?;
-    cn.execute("INSERT INTO users (id, name, age) VALUES (1, 'Haruna', 18)", params![])?;
-    cn.execute("INSERT INTO users (id, name, age) VALUES (1, 'Kirishima', 15)", params![])?;
+    cn.execute("INSERT INTO users (id, name, age) VALUES (?, ?, ?)", params![1, "Kongo", 20])?;
+    cn.execute("INSERT INTO users (id, name, age) VALUES (?, ?, ?)", params![1, "Hieai", 20])?;
+    cn.execute("INSERT INTO users (id, name, age) VALUES (?, ?, ?)", params![1, "Haruna", 18])?;
+    cn.execute("INSERT INTO users (id, name, age) VALUES (?, ?, ?)", params![1, "Kirishima", 15])?;
     Ok(())
 }
