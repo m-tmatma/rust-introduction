f404ea5a75aa74e3bff752aa620d315414996425
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-01-04 09:12:44 +0900

    execute_batch

--- chap15/code15-14/src/main.rs
+++ chap15/code15-14/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{Connection, Result};
+
+fn main() -> Result<()> {
+    let cn = Connection::open_in_memory()?;
+
+    cn.execute_batch(
+        "
+        CREATE TABLE users (id INTEGER, name TEXT, age INTEGER);
+        INSERT INTO users (id, name, age) VALUES (1, 'Kongo', 20);
+        INSERT INTO users (id, name, age) VALUES (1, 'Hieai', 20);
+        INSERT INTO users (id, name, age) VALUES (1, 'Haruna', 18);
+        INSERT INTO users (id, name, age) VALUES (1, 'Kirishima', 15);
+        "
+    )?;
+    Ok(())
 }
