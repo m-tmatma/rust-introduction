817426570c3f6cdc0815049b50170c2a62e0c6e0
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-01-04 09:21:50 +0900

    fix ids

--- chap15/code15-14/src/main.rs
+++ chap15/code15-14/src/main.rs
@@ -7,9 +7,9 @@ fn main() -> Result<()> {
         "
         CREATE TABLE users (id INTEGER, name TEXT, age INTEGER);
         INSERT INTO users (id, name, age) VALUES (1, 'Kongo', 20);
-        INSERT INTO users (id, name, age) VALUES (1, 'Hieai', 20);
-        INSERT INTO users (id, name, age) VALUES (1, 'Haruna', 18);
-        INSERT INTO users (id, name, age) VALUES (1, 'Kirishima', 15);
+        INSERT INTO users (id, name, age) VALUES (2, 'Hieai', 20);
+        INSERT INTO users (id, name, age) VALUES (3, 'Haruna', 18);
+        INSERT INTO users (id, name, age) VALUES (4, 'Kirishima', 15);
         "
     )?;
     Ok(())
