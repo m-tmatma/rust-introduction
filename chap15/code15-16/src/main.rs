1010f57592ee32d16b3bf28f9fe43afeeda0b97c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-01-04 09:29:51 +0900

    add

--- chap15/code15-16/src/main.rs
+++ chap15/code15-16/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{params, Connection, Result};
+
+fn main() -> Result<()> {
+    let cn = Connection::open_in_memory()?;
+    cn.execute("CREATE TABLE users (id INTEGER, name TEXT, age INTEGER)", params![])?;
+
+    let mut stmt = cn.prepare("INSERT INTO users (id, name, age) VALUES (?, ?, ?)")?;
+    stmt.execute(params![1, "Kongo", 20])?;
+    stmt.execute(params![2, "Hieai", 20])?;
+    stmt.execute(params![3, "Haruna", 18])?;
+    stmt.execute(params![4, "Kirishima", 15])?;
+
+    let mut stmt = cn.prepare("SELECT * FROM users")?;
+    let mut rows = stmt.query(params![])?;
+    while let Some(rows) = rows.next()? {
+        let id: i32 = rows.get(0)?;
+        let name: String = rows.get(1)?;
+        let age: i32 = rows.get(2)?;
+        println!("id: {}, name: {}, age: {}", id, name, age);
+    }
+    Ok(())
 }
